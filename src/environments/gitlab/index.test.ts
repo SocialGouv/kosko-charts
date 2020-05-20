4d3419e5c4770a54a4b7ef8c5347610616863cee
Douglas Duteil <douglasduteil@gmail.com>
2020-05-20 07:13:26 +0200

    feat: format preprod subdomain with CI_COMMIT_TAG

--- src/environments/gitlab/index.test.ts
+++ src/environments/gitlab/index.test.ts
@@ -1,40 +1,50 @@
 import env from "./index";
 
-test("should throw because of missing variables", () => {
-  expect(env).toThrowErrorMatchingSnapshot();
+test.each([
+  ["because of missing variables", {}],
+  [
+    "because of empty strings",
+    {
+      CI_ENVIRONMENT_NAME: "",
+      CI_ENVIRONMENT_SLUG: "",
+      CI_PROJECT_NAME: "",
+      CI_PROJECT_PATH_SLUG: "",
+      KUBE_INGRESS_BASE_DOMAIN: "",
+      KUBE_NAMESPACE: "",
+      CI_COMMIT_TAG: "",
+      PRODUCTION: "",
+    },
+  ],
+])("should throw %s", (_: string, testEnv?: NodeJS.ProcessEnv) => {
+  expect(() => env(testEnv)).toThrowErrorMatchingSnapshot();
 });
 
-test("should return the gitlab global env", () => {
-  expect(
-    env({
-      CI_ENVIRONMENT_NAME: "fabrique-dev",
-      CI_ENVIRONMENT_SLUG: "my-test",
-      CI_PROJECT_NAME: "sample",
-      CI_PROJECT_PATH_SLUG: "socialgouv-sample",
-      CI_REGISTRY_IMAGE:
-        "registry.gitlab.factory.social.gouv.fr/socialgouv/sample",
-      IMAGE_TAG: "x.y.z",
-      KUBE_INGRESS_BASE_DOMAIN: "dev.fabrique.social.gouv.fr",
-      KUBE_NAMESPACE: "sample-42-my-test",
-      RANCHER_PROJECT_ID: "c-kk8xm:p-4fxg8",
-    })
-  ).toMatchInlineSnapshot(`
-    Object {
-      "annotations": Object {
-        "app.gitlab.com/app": "socialgouv-sample",
-        "app.gitlab.com/env": "my-test",
-        "app.gitlab.com/env.name": "fabrique-dev",
-      },
-      "domain": "dev.fabrique.social.gouv.fr",
-      "labels": Object {
-        "application": "my-test-sample",
-        "owner": "sample",
-        "team": "sample",
-      },
-      "namespace": Object {
-        "name": "sample-42-my-test",
-      },
-      "subdomain": "my-test-sample",
-    }
-  `);
+const validEnv = {
+  CI_ENVIRONMENT_NAME: "fabrique-dev",
+  CI_ENVIRONMENT_SLUG: "my-test",
+  CI_PROJECT_NAME: "sample",
+  CI_PROJECT_PATH_SLUG: "socialgouv-sample",
+  CI_REGISTRY_IMAGE: "registry.gitlab.factory.social.gouv.fr/socialgouv/sample",
+  KUBE_INGRESS_BASE_DOMAIN: "dev.fabrique.social.gouv.fr",
+  KUBE_NAMESPACE: "sample-42-my-test",
+};
+test.each([
+  ["the gitlab global env", { ...validEnv }],
+  [
+    "tagged gitlab global env",
+    {
+      ...validEnv,
+      CI_COMMIT_TAG: "vX.Y.Z",
+    },
+  ],
+  [
+    "production gitlab global env",
+    {
+      ...validEnv,
+      CI_COMMIT_TAG: "vX.Y.Z",
+      PRODUCTION: "true",
+    },
+  ],
+])("should return %s", (_: string, testEnv?: NodeJS.ProcessEnv) => {
+  expect(env(testEnv)).toMatchSnapshot();
 });
--- src/environments/gitlab/index.ts
+++ src/environments/gitlab/index.ts
@@ -1,27 +1,35 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
 //
 
 import { fold } from "fp-ts/lib/Either";
 import { pipe } from "fp-ts/lib/pipeable";
-import * as t from "io-ts";
-import { NonEmptyString } from "io-ts-types/lib/NonEmptyString";
-import { failure } from "io-ts/lib/PathReporter";
+import { drawForest } from "fp-ts/lib/Tree";
+import * as D from "io-ts/lib/Decoder";
 
 import { GlobalEnvironment } from "@socialgouv/kosko-charts/types";
 
-const GitlabProcessEnv = t.type(
-  {
-    PRODUCTION: t.union([t.string, t.undefined]),
+export const NonEmptyString: D.Decoder<string> = D.refinement(
+  D.string,
+  (s: unknown): s is string => typeof s === "string" && s.length > 0,
+  "NonEmptyString"
+);
+
+const GitlabProcessEnv = D.intersection(
+  D.type({
     CI_ENVIRONMENT_NAME: NonEmptyString,
     CI_ENVIRONMENT_SLUG: NonEmptyString,
     CI_PROJECT_NAME: NonEmptyString,
     CI_PROJECT_PATH_SLUG: NonEmptyString,
     KUBE_INGRESS_BASE_DOMAIN: NonEmptyString,
     KUBE_NAMESPACE: NonEmptyString,
-  },
-  "process.env"
+  }),
+  D.partial({
+    CI_COMMIT_TAG: NonEmptyString,
+    PRODUCTION: D.string,
+  })
 );
 
-type GitlabProcessEnv = t.TypeOf<typeof GitlabProcessEnv>;
+type GitlabProcessEnv = D.TypeOf<typeof GitlabProcessEnv>;
 
 function assertValidEnv(env: typeof process.env): GitlabProcessEnv {
   /* eslint-disable @typescript-eslint/unbound-method */
@@ -29,22 +37,18 @@ function assertValidEnv(env: typeof process.env): GitlabProcessEnv {
     env,
     GitlabProcessEnv.decode,
     fold(
-      (errors) => {
-        throw new Error(
-          [
-            "Invalid config provided:",
-            "- " + failure(errors).join("\n- ").replace(/\//g, "."),
-          ].join("\n")
-        );
+      (errorForest) => {
+        throw new Error(["BadArgument:", drawForest(errorForest)].join(""));
       },
       (env) => ({
-        PRODUCTION: env.PRODUCTION,
+        CI_COMMIT_TAG: env.CI_COMMIT_TAG,
         CI_ENVIRONMENT_NAME: env.CI_ENVIRONMENT_NAME,
         CI_ENVIRONMENT_SLUG: env.CI_ENVIRONMENT_SLUG,
         CI_PROJECT_NAME: env.CI_PROJECT_NAME,
         CI_PROJECT_PATH_SLUG: env.CI_PROJECT_PATH_SLUG,
         KUBE_INGRESS_BASE_DOMAIN: env.KUBE_INGRESS_BASE_DOMAIN,
         KUBE_NAMESPACE: env.KUBE_NAMESPACE,
+        PRODUCTION: env.PRODUCTION,
       })
     )
   );
@@ -52,17 +56,23 @@ function assertValidEnv(env: typeof process.env): GitlabProcessEnv {
 
 export default (env = process.env): GlobalEnvironment => {
   const {
-    PRODUCTION,
+    CI_COMMIT_TAG,
     CI_ENVIRONMENT_NAME,
     CI_ENVIRONMENT_SLUG,
     CI_PROJECT_NAME,
     CI_PROJECT_PATH_SLUG,
     KUBE_INGRESS_BASE_DOMAIN,
     KUBE_NAMESPACE,
+    PRODUCTION,
   } = assertValidEnv(env);
 
   const isProductionCluster = Boolean(PRODUCTION);
-  const application = isProductionCluster ? CI_PROJECT_NAME : KUBE_NAMESPACE;
+
+  const application = isProductionCluster
+    ? CI_PROJECT_NAME
+    : CI_COMMIT_TAG !== undefined
+    ? `${CI_COMMIT_TAG.replace(/\./g, "-")}-${CI_PROJECT_NAME}`
+    : `${CI_ENVIRONMENT_SLUG}-${CI_PROJECT_NAME}`;
 
   return {
     namespace: {
