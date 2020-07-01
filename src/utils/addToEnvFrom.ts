56a8132c2190285b822f6a951283b07488074564
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2020-07-02 00:23:10 +0200

    feat: add utils/addToEnvFrom

--- src/utils/addToEnvFrom.ts
+++ src/utils/addToEnvFrom.ts
@@ -1,4 +1,7 @@
+<<<<<<< HEAD
 import { assert } from "@sindresorhus/is";
+=======
+>>>>>>> feat: add utils/addToEnvFrom
 import { Deployment } from "kubernetes-models/apps/v1/Deployment";
 import { EnvFromSource } from "kubernetes-models/v1/EnvFromSource";
 
