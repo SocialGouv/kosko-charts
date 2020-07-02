787a882dcfc48c4c05fbbfead66ed37022c9ecce
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2020-07-02 13:35:58 +0200

    feat(utils): add helperz (#65)
    Co-authored-by: Douglas Duteil <douglasduteil@gmail.com>

---
+++ src/utils/addToEnvFrom.ts
@@ -0,0 +1,21 @@
+import { assert } from "@sindresorhus/is";
+import { Deployment } from "kubernetes-models/apps/v1/Deployment";
+import { EnvFromSource } from "kubernetes-models/v1/EnvFromSource";
+
+interface AddToContainerParams {
+  deployment: Deployment;
+  data: EnvFromSource[];
+  containerIndex?: number;
+}
+
+export const addToEnvFrom = ({
+  deployment,
+  data,
+  containerIndex = 0,
+}: AddToContainerParams): void => {
+  assert.object(deployment.spec);
+  assert.object(deployment.spec.template.spec);
+  const container = deployment.spec.template.spec.containers[containerIndex];
+  container.envFrom = container.envFrom ?? [];
+  container.envFrom.push(...data);
+};
