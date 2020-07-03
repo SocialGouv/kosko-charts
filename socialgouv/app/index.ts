31f51e15dc6d1af1565f3e845f16d56827ca4deb
Douglas Duteil <douglasduteil@gmail.com>
2020-07-03 16:17:27 +0200

    feat: gen the app too

---
+++ socialgouv/app/index.ts
@@ -0,0 +1,20 @@
+import { SocialGouvAppParams } from "./params";
+import { create as createNamespace } from "@socialgouv/kosko-charts/components/namespace";
+import { create as createApp } from "@socialgouv/kosko-charts/components/app";
+import gitlabGlobalEnv from "@socialgouv/kosko-charts/environments/gitlab";
+import gitlabNamespaceEnv from "@socialgouv/kosko-charts/environments/gitlab/namespaces";
+import gitlabAppEnv from "@socialgouv/kosko-charts/environments/gitlab/app";
+import { merge } from "@socialgouv/kosko-charts/utils/merge";
+
+export function create(params: SocialGouvAppParams) {
+  const globalParams = gitlabGlobalEnv(process.env);
+
+  const namespaceParams = gitlabNamespaceEnv(process.env);
+  const { namespace } = createNamespace(merge(globalParams, namespaceParams));
+
+  const appParams = gitlabAppEnv(process.env);
+  const { deployment, ingress, service } = createApp(
+    merge(globalParams, appParams)
+  );
+  return [namespace, deployment, ingress, service];
+}
