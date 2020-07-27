fa735b441e39eb1a76df51e3b7abbcffda12cbdf
Douglas Duteil <douglasduteil@gmail.com>
2020-07-27 15:20:31 +0200

    build(template): remove old env files

--- templates/simple/components/www.ts
+++ templates/simple/components/www.ts
@@ -1,5 +1,20 @@
 import env from "@kosko/env";
 import { create } from "@socialgouv/kosko-charts/components/app";
 
-const { deployment, ingress, service } = create(env.component("www"));
-export default [deployment, ingress, service];
+const manifests = create("app", {
+  config: {
+    containerPort: 3030,
+    image: "",
+    limits: {
+      cpu: "50m",
+      memory: "128Mi",
+    },
+    requests: {
+      cpu: "1m",
+      memory: "64Mi",
+    },
+  },
+  env,
+});
+
+export default manifests;
