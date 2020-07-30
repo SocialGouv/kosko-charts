e82f76e84564ccee7b1951e89335038824ba118a
Douglas Duteil <douglasduteil@gmail.com>
2020-07-30 09:27:08 +0200

    test(azure-pg): ensure to have a pg.sealed-secret.yaml to work (2)

---
+++ src/components/azure-pg/index.test.ts
@@ -0,0 +1,47 @@
+import { Environment } from "@kosko/env";
+import { mkdir, writeFile } from "fs/promises";
+import { directory } from "tempy";
+
+import { create } from "./index";
+
+const gitlabEnv = {
+  CI_ENVIRONMENT_NAME: "fabrique-dev",
+  CI_ENVIRONMENT_SLUG: "my-test",
+  CI_PROJECT_NAME: "sample",
+  CI_PROJECT_PATH_SLUG: "socialgouv-sample",
+  CI_REGISTRY_IMAGE: "registry.gitlab.factory.social.gouv.fr/socialgouv/sample",
+  KUBE_INGRESS_BASE_DOMAIN: "dev.fabrique.social.gouv.fr",
+  KUBE_NAMESPACE: "sample-42-my-test",
+};
+
+test("should throw because of a missing CI_PROJECT_NAME", () => {
+  const env = new Environment("/tmp");
+  expect(() => create({ env })).toThrowError(
+    "Missing process.env.CI_PROJECT_NAME"
+  );
+});
+
+test("should throw because of a missing envs", () => {
+  process.env.CI_PROJECT_NAME = "sample-next-app";
+  const env = new Environment("/tmp");
+  expect(() => create({ env })).toThrowErrorMatchingSnapshot();
+});
+
+test("should throw because of a missing pg.sealed-secret.yaml", () => {
+  Object.assign(process.env, gitlabEnv);
+  const env = new Environment("/tmp");
+  expect(() => create({ env })).toThrowError("Missing pg.sealed-secret.yaml");
+});
+
+test("should return create an job", async () => {
+  Object.assign(process.env, gitlabEnv);
+  const cwd = directory();
+  const env = new Environment(cwd);
+  env.env = "dev";
+  await mkdir(`${cwd}/environments/dev`, { recursive: true });
+  await writeFile(
+    `${cwd}/environments/dev/pg.sealed-secret.yaml`,
+    "---\napiVersion: v1\nkind: ConfigMap"
+  );
+  expect(create({ env })).toMatchSnapshot();
+});
