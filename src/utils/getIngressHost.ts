ce60a7919312464b0f208cfb8f1df690ab489d73
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2020-08-05 18:04:20 +0200

    feat: add utils: getIngressHost and getManifestByKind (#108)

---
+++ src/utils/getIngressHost.ts
@@ -0,0 +1,14 @@
+import { ok } from "assert";
+import { Ingress } from "kubernetes-models/api/networking/v1beta1/Ingress";
+
+import { getManifestByKind } from "./getManifestByKind";
+
+export const getIngressHost = (manifests: { kind: string }[]) => {
+  //@ts-expect-error
+  const ingress = getManifestByKind(manifests, Ingress) as Ingress;
+  ok(ingress);
+  ok(ingress.spec);
+  ok(ingress.spec.rules);
+  ok(ingress.spec.rules[0]);
+  return ingress.spec.rules[0].host;
+};
