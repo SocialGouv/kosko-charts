79fe103bc718af265cb37ac98f921eb7a44a65e5
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2020-08-07 18:44:28 +0200

    fix(k8s): restore waitForPg delays

--- src/utils/waitForPostgres.ts
+++ src/utils/waitForPostgres.ts
@@ -7,12 +7,12 @@ interface WaitForPostgresParams {
 }
 
 const script = `
-retry=5; # 5s * (12 * 10) = 10min
+retry=120; # 5s * (12 * 10) = 10min
 while ! psql -c "SELECT VERSION();" > /dev/null 2> /dev/null && [ $retry -gt 0 ];
   do
     echo "Waiting for Postgres to go Green ($(( retry )))" ;
     retry=$(( $retry-1 ));
-    sleep 1s ;
+    sleep 5s ;
   done ;
 if [ $retry -eq 0 ]; then
     echo "Not Ready";
