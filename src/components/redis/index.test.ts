3c55190dd7b537b6853e8b093b8ff7f85c443a2b
Douglas Duteil <douglasduteil@gmail.com>
2020-10-20 15:39:34 +0200

    test(redis): add more redis test (2)

---
+++ src/components/redis/index.test.ts
@@ -0,0 +1,80 @@
+import { Environment } from "@kosko/env";
+
+import { create } from "./index";
+
+jest.mock("@socialgouv/kosko-charts/components/app", () => ({
+  create: jest.fn().mockImplementation((...args: unknown[]) => args),
+}));
+
+beforeEach(() => {
+  jest.resetModules();
+});
+
+test("should create app with redis config", () => {
+  const env = new Environment("/tmp");
+  const manifest = create("redis", { env });
+  expect(manifest).toMatchInlineSnapshot(`
+    Array [
+      "redis",
+      Object {
+        "config": Object {
+          "container": Object {
+            "livenessProbe": Object {
+              "exec": Object {
+                "command": Array [
+                  "sh",
+                  "-c",
+                  "redis-cli ping",
+                ],
+              },
+            },
+            "readinessProbe": Object {
+              "exec": Object {
+                "command": Array [
+                  "sh",
+                  "-c",
+                  "redis-cli ping",
+                ],
+              },
+            },
+            "startupProbe": Object {
+              "exec": Object {
+                "command": Array [
+                  "sh",
+                  "-c",
+                  "redis-cli ping",
+                ],
+              },
+            },
+          },
+          "containerPort": 6379,
+          "image": "redis:6.0.5-alpine3.12",
+          "ingress": false,
+          "subDomainPrefix": "redis-",
+        },
+        "deployment": Object {
+          "labels": Object {
+            "component": "redis",
+          },
+        },
+        "env": Environment {
+          "cwd": "/tmp",
+          "paths": Object {
+            "component": "environments/#{environment}/#{component}",
+            "global": "environments/#{environment}",
+          },
+          "reducers": Array [
+            Object {
+              "name": "global",
+              "reduce": [Function],
+            },
+            Object {
+              "name": "component",
+              "reduce": [Function],
+            },
+          ],
+        },
+      },
+    ]
+  `);
+});
