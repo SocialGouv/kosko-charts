66b5565776658ad15f53473269f8e36e3af573f8
Douglas Duteil <douglasduteil@gmail.com>
2020-10-20 14:11:50 +0200

    feat(template): add dummy pg component

---
+++ templates/pgweb/components/pg.ts
@@ -0,0 +1,29 @@
+import env from "@kosko/env";
+import { SealedSecret } from "@kubernetes-models/sealed-secrets/bitnami.com/v1alpha1/SealedSecret";
+import { create } from "@socialgouv/kosko-charts/components/azure-pg";
+import gitlab from "@socialgouv/kosko-charts/environments/gitlab";
+import { loadYaml } from "@socialgouv/kosko-charts/utils/getEnvironmentComponent";
+import { updateMetadata } from "@socialgouv/kosko-charts/utils/updateMetadata";
+
+export default () => {
+  if (env.env === "dev") {
+    return create({
+      env,
+    });
+  }
+
+  // in prod/preprod, we try to add a fixed sealed-secret
+  const secret = loadYaml<SealedSecret>(env, `pg-user.sealed-secret.yaml`);
+  if (!secret) {
+    return [];
+  }
+
+  const envParams = gitlab(process.env);
+  // add gitlab annotations
+  updateMetadata(secret, {
+    annotations: envParams.annotations ?? {},
+    labels: envParams.labels ?? {},
+    namespace: envParams.namespace,
+  });
+  return [secret];
+};
