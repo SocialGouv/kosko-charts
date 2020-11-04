9f6be2392ee715e2ddb4d9ffe26696fa9bdc3d74
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2020-11-04 15:52:15 +0100

    feat(azure-pg): add drop-autodevops-dbs job

---
+++ src/components/azure-pg/drop-autodevops-dbs.job.ts
@@ -0,0 +1,50 @@
+import { Job } from "kubernetes-models/batch/v1/Job";
+
+export const dropDbJob = ({
+  //@ts-expect-error
+  database,
+  //@ts-expect-error
+  user,
+  secretRefName = `azure-pg-admin-user`,
+}): Job => {
+  return new Job({
+    metadata: {
+      name: `drop-azure-autodevops-dbs-${process.env.CI_COMMIT_SHORT_SHA}`,
+    },
+    spec: {
+      backoffLimit: 0,
+      template: {
+        metadata: {},
+        spec: {
+          containers: [
+            {
+              command: ["drop-autodevops-dbs"],
+              envFrom: [
+                {
+                  secretRef: {
+                    name: secretRefName,
+                  },
+                },
+              ],
+              image:
+                "registry.gitlab.factory.social.gouv.fr/socialgouv/docker/azure-db:0.28.0",
+              imagePullPolicy: "IfNotPresent",
+              name: "drop-autodevops-dbs",
+              resources: {
+                limits: {
+                  cpu: "300m",
+                  memory: "256Mi",
+                },
+                requests: {
+                  cpu: "100m",
+                  memory: "64Mi",
+                },
+              },
+            },
+          ],
+          restartPolicy: "Never",
+        },
+      },
+    },
+  });
+};
