989a4e7e279b985bddbacb01f951d06da49374b1
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2020-11-05 10:30:04 +0100

    fix: missing process.env.CI_COMMIT_SHORT_SHA

--- src/components/azure-pg/drop-autodevops-dbs.test.ts
+++ src/components/azure-pg/drop-autodevops-dbs.test.ts
@@ -1,5 +1,6 @@
-/* eslint-disable sort-keys-fix/sort-keys-fix */
 import { dropAutodevopsDbsJob } from "./drop-autodevops-dbs.job";
+
+process.env.CI_COMMIT_SHORT_SHA = "b123a99";
 
 test("should use some-secret for running drop-autodevops-dbs", () => {
   expect(
