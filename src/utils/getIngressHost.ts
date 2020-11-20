d98c33edfa244ff5d6a6ee0147a923469b1e0477
Douglas DUTEIL <douglasduteil@gmail.com>
2020-11-20 14:32:49 +0100

    styles(utils): explicit return type

--- src/utils/getIngressHost.ts
+++ src/utils/getIngressHost.ts
@@ -3,7 +3,9 @@ import { Ingress } from "kubernetes-models/api/networking/v1beta1/Ingress";
 
 import { getManifestByKind } from "./getManifestByKind";
 
-export const getIngressHost = (manifests: { kind: string }[]) => {
+export const getIngressHost = (
+  manifests: { kind: string }[]
+): string | undefined => {
   //@ts-expect-error
   const ingress = getManifestByKind(manifests, Ingress) as Ingress;
   ok(ingress);
