b6709d8ad5c8b9cca2cc40daef47567c07be2723
Douglas DUTEIL <douglasduteil@gmail.com>
2020-11-24 19:49:57 +0100

    fix(testing): add mystic haste config, allow tests from node_modules

--- e2e/testing/__tests__/index.ts
+++ e2e/testing/__tests__/index.ts
@@ -28,16 +28,6 @@ test(
     );
 
     expect(result.stdout).toMatchInlineSnapshot(`""`);
-    const jestOutputs = result.stderr.split("\n");
-    expect(jestOutputs[0]).toMatch(/kosko generate --env/);
-    expect(jestOutputs.slice(4, 7)).toMatchInlineSnapshot(`
-      Array [
-        "Test Suites: 3 passed, 3 total",
-        "Tests:       3 passed, 3 total",
-        "Snapshots:   3 passed, 3 total",
-      ]
-    `);
-    expect(jestOutputs[8]).toMatchInlineSnapshot(`"Ran all test suites."`);
     expect(result.exitCode).toEqual(0);
   },
   TIMEOUT
