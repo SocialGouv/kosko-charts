dfd25c789d3ec6b84fd6d61d937abee3a2fbdd1b
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2020-12-09 13:20:59 +0100

    fix: fix config overrides (#317)
    * tests: add app tests
    
    * fix(app): change config priority order
    
    Co-authored-by: Douglas DUTEIL <douglasduteil@gmail.com>

---
+++ src/components/app/index.test.ts
@@ -0,0 +1,71 @@
+import { Environment } from "@kosko/env";
+import { project } from "@socialgouv/kosko-charts/testing/fake/gitlab-ci.env";
+import { promises } from "fs";
+import { directory } from "tempy";
+
+import { create } from "./index";
+
+jest.mock("@socialgouv/kosko-charts/environments/gitlab", () => ({
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  __esModule: true,
+  default: () => ({
+    annotations: {
+      "app.gitlab.com/app": "socialgouv-sample",
+      "app.gitlab.com/env": "my-test",
+    },
+    domain: "fabrique.social.gouv.fr",
+    labels: {
+      application: "sample",
+      owner: "sample",
+      team: "sample",
+    },
+    namespace: { name: "sample-42-my-test" },
+    subdomain: "sample",
+  }),
+}));
+
+beforeEach(() => {
+  jest.resetModules();
+});
+
+test("should throw because of a missing envs", () => {
+  const env = new Environment("/tmp");
+  expect(() => create("app", { env })).toThrowErrorMatchingSnapshot();
+});
+
+test("should return dev manifests", async () => {
+  const gitlabEnv = project("sample").dev;
+  Object.assign(process.env, gitlabEnv);
+  const cwd = directory();
+  const env = new Environment(cwd);
+  env.env = "dev";
+  await promises.mkdir(`${cwd}/environments/dev`, { recursive: true });
+  expect(create("app", { env })).toMatchSnapshot();
+});
+
+test("should return prod manifests", async () => {
+  const gitlabEnv = project("sample").prod;
+  Object.assign(process.env, gitlabEnv);
+  const cwd = directory();
+  const env = new Environment(cwd);
+  env.env = "prod";
+  await promises.mkdir(`${cwd}/environments/prod`, { recursive: true });
+  expect(create("app", { env })).toMatchSnapshot();
+});
+
+test("should return prod manifests with custom subdomain", async () => {
+  const gitlabEnv = project("sample").prod;
+  Object.assign(process.env, gitlabEnv);
+  const cwd = directory();
+  const env = new Environment(cwd);
+  env.env = "prod";
+  await promises.mkdir(`${cwd}/environments/prod`, { recursive: true });
+  expect(
+    create("app", {
+      config: {
+        subdomain: "another",
+      },
+      env,
+    })
+  ).toMatchSnapshot();
+});
