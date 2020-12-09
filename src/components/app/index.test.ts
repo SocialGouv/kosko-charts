1b2ee4794919c7197f4ba314d3253c0b5debfcb0
Douglas DUTEIL <douglasduteil@gmail.com>
2020-12-09 12:38:05 +0100

    fix(app): change config priority order

--- src/components/app/index.test.ts
+++ src/components/app/index.test.ts
@@ -1,37 +1,9 @@
 import { Environment } from "@kosko/env";
+import { project } from "@socialgouv/kosko-charts/testing/fake/gitlab-ci.env";
 import { promises } from "fs";
 import { directory } from "tempy";
 
 import { create } from "./index";
-
-const gitlabEnv = {
-  CI_COMMIT_BEFORE_SHA: "0000000000000000000000000000000000000000",
-  CI_COMMIT_DESCRIPTION: "",
-  CI_COMMIT_REF_PROTECTED: "false",
-  CI_COMMIT_SHA: "0123456789abcdefghijklmnopqrstuvwxyz0123",
-  CI_COMMIT_SHORT_SHA: "0123456",
-  CI_COMMIT_TIMESTAMP: "1111-11-11T11:11:11+00:00",
-  CI_ENVIRONMENT_URL: "https://sample-next-app.fabrique.social.gouv.fr",
-  CI_PROJECT_DIR: "/builds/SocialGouv/sample-next-app",
-  CI_PROJECT_ID: "85",
-  CI_PROJECT_NAME: "sample-next-app",
-  CI_PROJECT_NAMESPACE: "SocialGouv",
-  CI_PROJECT_PATH_SLUG: "socialgouv-sample-next-app",
-  CI_PROJECT_PATH: "SocialGouv/sample-next-app",
-  CI_PROJECT_ROOT_NAMESPACE: "SocialGouv",
-  CI_PROJECT_TITLE: "sample-next-app",
-  CI_PROJECT_URL:
-    "https://gitlab.factory.social.gouv.fr/SocialGouv/sample-next-app",
-  CI_PROJECT_VISIBILITY: "public",
-  CI_REGISTRY_IMAGE:
-    "registry.gitlab.factory.social.gouv.fr/socialgouv/sample-next-app",
-  CI_REGISTRY_USER: "gitlab-ci-token",
-  CI_REPOSITORY_URL:
-    "https://gitlab-ci-token:[MASKED]@gitlab.factory.social.gouv.fr/SocialGouv/sample-next-app.git",
-
-  CI_COMMIT_TAG: "",
-  PRODUCTION: "",
-};
 
 jest.mock("@socialgouv/kosko-charts/environments/gitlab", () => ({
   // eslint-disable-next-line @typescript-eslint/naming-convention
@@ -42,14 +14,13 @@ jest.mock("@socialgouv/kosko-charts/environments/gitlab", () => ({
       "app.gitlab.com/env": "my-test",
     },
     domain: "fabrique.social.gouv.fr",
-    subdomain: "sample",
     labels: {
       application: "sample",
       owner: "sample",
       team: "sample",
     },
-
     namespace: { name: "sample-42-my-test" },
+    subdomain: "sample",
   }),
 }));
 
@@ -63,6 +34,7 @@ test("should throw because of a missing envs", () => {
 });
 
 test("should return dev manifests", async () => {
+  const gitlabEnv = project("sample").dev;
   Object.assign(process.env, gitlabEnv);
   const cwd = directory();
   const env = new Environment(cwd);
@@ -72,10 +44,8 @@ test("should return dev manifests", async () => {
 });
 
 test("should return prod manifests", async () => {
+  const gitlabEnv = project("sample").prod;
   Object.assign(process.env, gitlabEnv);
-  Object.assign(process.env, {
-    PRODUCTION: true,
-  });
   const cwd = directory();
   const env = new Environment(cwd);
   env.env = "prod";
@@ -84,20 +54,18 @@ test("should return prod manifests", async () => {
 });
 
 test("should return prod manifests with custom subdomain", async () => {
+  const gitlabEnv = project("sample").prod;
   Object.assign(process.env, gitlabEnv);
-  Object.assign(process.env, {
-    PRODUCTION: true,
-  });
   const cwd = directory();
   const env = new Environment(cwd);
   env.env = "prod";
   await promises.mkdir(`${cwd}/environments/prod`, { recursive: true });
   expect(
     create("app", {
-      env,
       config: {
         subdomain: "another",
       },
+      env,
     })
   ).toMatchSnapshot();
 });
