8779cc318f9847a53a671a8aec255567d344c49f
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2020-12-30 11:51:35 +0100

    fix: use merge

--- src/utils/addInitContainerCommand.ts
+++ src/utils/addInitContainerCommand.ts
@@ -3,6 +3,8 @@ import { ok } from "assert";
 import type { Deployment } from "kubernetes-models/apps/v1/Deployment";
 import { Container } from "kubernetes-models/v1/Container";
 
+import { merge } from "./@kosko/env/merge";
+
 export const addInitContainerCommand = (
   deployment: Deployment,
   container: Partial<Container>
@@ -11,24 +13,28 @@ export const addInitContainerCommand = (
   ok(deployment.spec.template);
   ok(deployment.spec.template.spec);
   const image = deployment.spec.template.spec.containers[0].image;
-  const initContainer = new Container({
-    args: [],
-    command: ["hostname"],
-    image,
-    imagePullPolicy: "Always",
-    name: "init",
-    resources: {
-      limits: {
-        cpu: "500m",
-        memory: "512Mi",
+  const initContainer = new Container(
+    merge(
+      {
+        args: [],
+        command: ["/bin/bash"], // dummy
+        image,
+        imagePullPolicy: "Always",
+        name: "init",
+        resources: {
+          limits: {
+            cpu: "500m",
+            memory: "512Mi",
+          },
+          requests: {
+            cpu: "100m",
+            memory: "128Mi",
+          },
+        },
       },
-      requests: {
-        cpu: "100m",
-        memory: "128Mi",
-      },
-    },
-    ...container,
-  });
+      container
+    )
+  );
 
   addInitContainer(deployment, initContainer);
 
