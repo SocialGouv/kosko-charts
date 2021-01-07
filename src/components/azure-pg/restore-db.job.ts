426323993c00ce02de87a00cd6efa53fecd36741
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2021-01-07 10:07:52 +0100

    fix(azure-db-job): fix restore

--- src/components/azure-pg/restore-db.job.ts
+++ src/components/azure-pg/restore-db.job.ts
@@ -45,7 +45,7 @@ pg_restore \
 
 psql -v ON_ERROR_STOP=1 \${PGDATABASE} -c "ALTER SCHEMA public owner to \${OWNER};"
 
-[ -f "/mnt/scripts/post-restore.sql" ] && psql \${PGDATABASE} -v ON_ERROR_STOP=1 -f -a /mnt/scripts/post-restore.sql
+[ -f "/mnt/scripts/post-restore.sql" ] && psql -v ON_ERROR_STOP=1 -a < /mnt/scripts/post-restore.sql
 `;
 
 const getProjectSecretNamespace = (project: string) => `${project}-secret`;
