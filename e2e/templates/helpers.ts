aed1556122487a941e8c8d03e34d4a09f4659f72
Douglas DUTEIL <douglasduteil@gmail.com>
2021-01-11 12:27:52 +0100

    e2e: inc timeout to 1min

--- e2e/templates/helpers.ts
+++ e2e/templates/helpers.ts
@@ -4,7 +4,7 @@ export const ROOT = resolve(__dirname, "../..");
 export const BIN_FOLDER = resolve(__dirname, ROOT, "node_modules/.bin");
 export const KOSKO_BIN = resolve(BIN_FOLDER, "kosko");
 
-export const TIMEOUT = 25_000; // = 1000 * 25s
+export const TIMEOUT = 60_000; // = 1000 * 60s
 
 export const template = (testFilename: string): string => {
   return resolve(__dirname, ROOT, "templates/", testFilename);
