bfbb42ed29bb74b3cce346950e70b7d48c9b1f69
Douglas Duteil <douglasduteil@gmail.com>
2021-01-29 12:06:58 +0100

    Update index.ts

--- src/components/redis/index.ts
+++ src/components/redis/index.ts
@@ -6,8 +6,8 @@ import { merge } from "@socialgouv/kosko-charts/utils/@kosko/env/merge";
 
 import type { DeploymentParams } from "../../utils/createDeployment";
 
-// renovate: datasource=docker depName=redis versioning=6.0.10
-const REDIS_VERSION = "6.0.10";
+// renovate: datasource=docker depName=redis versioning=6.0.10-alpine3.12
+const REDIS_VERSION = "6.0.10-alpine3.12";
 
 const redisDeploymentParams: Partial<
   Omit<DeploymentParams, "containerPort">
