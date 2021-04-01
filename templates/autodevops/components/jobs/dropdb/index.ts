03ae9d54f1c7998e854502be7e6cc6d786804bd4
Douglas DUTEIL <douglasduteil@gmail.com>
2021-04-01 22:16:22 +0200

    feat(autodevops): import PG_ENVIRONMENT_SLUG code

--- templates/autodevops/components/jobs/dropdb/index.ts
+++ templates/autodevops/components/jobs/dropdb/index.ts
@@ -1,8 +1,9 @@
 import { dropDbJob } from "@socialgouv/kosko-charts/components/azure-pg/drop-db.job";
 import { getDevDatabaseParameters } from "@socialgouv/kosko-charts/components/azure-pg/params";
+import { PG_ENVIRONMENT_SLUG } from "@socialgouv/kosko-charts/utils/PG_ENVIRONMENT_SLUG";
 import { ok } from "assert";
 
-const suffix = process.env.ENVIRONMENT_SLUG ?? process.env.CI_ENVIRONMENT_SLUG;
+const suffix = process.env.ENVIRONMENT_SLUG ?? PG_ENVIRONMENT_SLUG;
 
 ok(
   suffix,
@@ -14,5 +15,5 @@ export default dropDbJob(
   // HACK(douglasduteil): native gitlab ci/cd "support"
   // The CI_ENVIRONMENT_SLUG contants "-"
   // Postrges doesn't like "-" on user/database names
-  getDevDatabaseParameters({ suffix: suffix.replace(/-/g, "") })
+  getDevDatabaseParameters({ suffix })
 );
