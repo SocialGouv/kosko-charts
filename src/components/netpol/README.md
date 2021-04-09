8ec03e2522dd6e0fcb85487f8b2bbf7692bc22e1
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2021-04-09 08:17:01 +0200

    feat: add components.netpol

---
+++ src/components/netpol/README.md
@@ -0,0 +1,15 @@
+# Namespace Component
+
+Add a network policy to protect a given namespace
+
+## Usage
+
+
+```ts
+// in .k8s/components/netpol.ts
+import { create } from "@socialgouv/kosko-charts/components/netpol";
+import env from "@kosko/env";
+
+const netpol = create("my-namespace")
+export default netpol;
+```
---
+++ src/components/netpol/index.test.ts
@@ -0,0 +1,17 @@
+//
+
+
+beforeEach(() => {
+  jest.resetModules();
+});
+
+test("should create a network policy", async () => {
+  jest.doMock(
+    "@socialgouv/kosko-charts/environments/gitlab",
+    () => gitlabModuleMock
+  );
+
+  const { create } = await import("./index");
+  const netpol = create("some-app");
+  expect(netpol).toMatchSnapshot();
+});
