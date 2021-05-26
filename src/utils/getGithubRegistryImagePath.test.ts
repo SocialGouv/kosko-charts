75485f35396cd62bb41d160b03145b63b4332079
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2021-05-26 15:44:18 +0200

    Update src/utils/getGithubRegistryImagePath.test.ts
    Co-authored-by: Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>

--- src/utils/getGithubRegistryImagePath.test.ts
+++ src/utils/getGithubRegistryImagePath.test.ts
@@ -4,7 +4,7 @@ beforeEach(() => {
   process.env.CI_COMMIT_SHA = "2780ce341781678fe1f460742e87312fbabd827f";
 });
 
-test("custom harbor project with param with commit sha", () => {
+test("custom ghcr project with param with commit sha", () => {
   expect(
     getGithubRegistryImagePath({ name: "frontend", project: "some-project" })
   ).toStrictEqual(
