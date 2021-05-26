8ea85d3eb2420dc529f5331ae0c8da1130db2167
gary-van-woerkens <gary.van-woerkens@sg.social.gouv.fr>
2021-05-26 14:47:27 +0200

    Add CI_COMMIT_TAG test.

--- src/utils/getGithubRegistryImagePath.test.ts
+++ src/utils/getGithubRegistryImagePath.test.ts
@@ -11,3 +11,10 @@ test("custom harbor project with param with commit sha", () => {
     "ghcr.io/socialgouv/some-project/frontend:2780ce341781678fe1f460742e87312fbabd827f"
   );
 });
+
+test("custom registry with tag", () => {
+  process.env.CI_COMMIT_TAG = "v1.2.3";
+  expect(getGithubRegistryImagePath({ name: "frontend", project: "some-project" })).toStrictEqual(
+    "ghcr.io/socialgouv/some-project/frontend:1.2.3"
+  );
+});
