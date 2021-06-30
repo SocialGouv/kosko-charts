df7434498f3f14622ccacee7e7a7386b322bd6f0
Douglas Duteil <douglasduteil@gmail.com>
2021-06-30 14:37:02 +0200

    feat(azure-storage): add dynamic fileshare pvc (#584)
    * feat(azure-storage): add dynamic fileshare pvc
    
    * chore: use interface instead of forced class

---
+++ src/components/azure-storage/fileshare.pvc.ts
@@ -0,0 +1,33 @@
+import { assert } from "@sindresorhus/is";
+import gitlab from "@socialgouv/kosko-charts/environments/gitlab";
+import { merge } from "@socialgouv/kosko-charts/utils/@kosko/env/merge";
+import type { IObjectMeta } from "kubernetes-models/apimachinery/pkg/apis/meta/v1";
+import type { IResourceRequirements } from "kubernetes-models/v1";
+import { PersistentVolumeClaim } from "kubernetes-models/v1";
+
+export function fileSharePersistentVolumeClaim({
+  metadata,
+  resources,
+}: {
+  metadata: IObjectMeta;
+  resources: IResourceRequirements;
+}): PersistentVolumeClaim {
+  assert.nonEmptyString(metadata.name);
+  const globalEnv = gitlab(process.env);
+
+  return new PersistentVolumeClaim({
+    metadata: merge(
+      {
+        annotations: globalEnv.annotations,
+        labels: globalEnv.labels,
+        namespace: globalEnv.namespace.name,
+      },
+      metadata
+    ),
+    spec: {
+      accessModes: ["ReadWriteMany"],
+      resources,
+      storageClassName: "azurefile",
+    },
+  });
+}
