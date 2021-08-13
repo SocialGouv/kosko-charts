b299e3c24f387e56deec45eb5adb2d2285205917
gary-van-woerkens <gary.van-woerkens@sg.social.gouv.fr>
2021-08-13 10:42:32 +0200

    fix: Add ghcr for hasura autodevops.

--- e2e/templates/autodevops/hasura/__tests__/kosko generate --env dev.ts
+++ e2e/templates/autodevops/hasura/__tests__/kosko generate --env dev.ts
@@ -57,6 +57,30 @@ test(
   TIMEOUT
 );
 
+test(
+  "hasura ghcr: kosko generate --dev",
+  async () => {
+    const gitlabEnv = config({
+      path: resolve(cwd, "./environments/.github-actions.env"),
+    }).parsed;
+
+    const env = {
+      ...gitlabEnv,
+      SOCIALGOUV_CONFIG_PATH: __dirname + "/config-ghcr.json",
+    };
+
+    // Required to allow seemless integration code example
+    const result = await execa.node(KOSKO_BIN, ["generate", "--env", "dev"], {
+      cwd,
+      env,
+    });
+
+    expect(result.stdout).toMatchSnapshot();
+    expect(result.exitCode).toEqual(0);
+  },
+  TIMEOUT
+);
+
 // //
 
 // import { getEnvManifests } from "@socialgouv/kosko-charts/testing"
--- templates/autodevops/components/hasura.ts
+++ templates/autodevops/components/hasura.ts
@@ -1,18 +1,24 @@
 import env from "@kosko/env";
 import { create } from "@socialgouv/kosko-charts/components/hasura";
+import { getGithubRegistryImagePath } from "@socialgouv/kosko-charts/utils/getGithubRegistryImagePath";
 import { getHarborImagePath } from "@socialgouv/kosko-charts/utils/getHarborImagePath";
 
 import type { Manifests } from "../types/config";
 import Config from "../utils/config";
 
 export default async (): Manifests => {
-  const { name, hasura } = await Config();
+  const { hasura, name: appName, registry } = await Config();
+
+  const name = "hasura";
+
+  const image =
+    registry === "ghcr"
+      ? getGithubRegistryImagePath({ name, project: appName })
+      : getHarborImagePath({ name: `${appName}-hasura` });
 
   const config = {
     config: { ingress: hasura === "exposed" },
-    deployment: {
-      image: getHarborImagePath({ name: `${name}-hasura` }),
-    },
+    deployment: { image },
     env,
   };
 
