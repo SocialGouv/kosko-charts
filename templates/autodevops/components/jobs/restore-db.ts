57fef26909e03f5c354f9fe0a66e10773ebe6ad9
Douglas DUTEIL <douglasduteil@gmail.com>
2021-09-03 14:58:53 +0200

    feat(autodevops): restore use env.metadata.git.remote

--- templates/autodevops/components/jobs/restore-db.ts
+++ templates/autodevops/components/jobs/restore-db.ts
@@ -1,4 +1,5 @@
 import { restoreDbFromFile } from "@socialgouv/kosko-charts/components/azure-pg";
+import ci from "@socialgouv/kosko-charts/environments";
 import type { IJob } from "kubernetes-models/batch/v1";
 
 import Config from "../../utils/config";
@@ -6,9 +7,14 @@ import Config from "../../utils/config";
 export default async (): Promise<IJob | never[]> => {
   const { name, restoreDb } = await Config();
 
+  const env = ci(process.env);
+
   if (!restoreDb) {
     return Promise.resolve([]);
   }
 
-  return restoreDbFromFile(name, { filePath: restoreDb, repository: name });
+  return restoreDbFromFile(name, {
+    filePath: restoreDb,
+    repository: `${env.metadata.git.remote}`,
+  });
 };
