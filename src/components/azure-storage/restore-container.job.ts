2ce7d48fc4c6d2ef1568dcb85babbc458ec34ea6
Douglas DUTEIL <douglasduteil@gmail.com>
2021-09-09 18:31:23 +0200

    refactor: restore-container job

--- src/components/azure-storage/restore-container.job.ts
+++ src/components/azure-storage/restore-container.job.ts
@@ -2,46 +2,50 @@ import environments from "@socialgouv/kosko-charts/environments";
 import { generate } from "@socialgouv/kosko-charts/utils/environmentSlug";
 import { readFileSync } from "fs";
 import { Job } from "kubernetes-models/batch/v1/Job";
-import type { EnvFromSource } from "kubernetes-models/v1/EnvFromSource";
-import { EnvVar } from "kubernetes-models/v1/EnvVar";
 import { join } from "path";
 
-interface RestoreJobArgs {
-  project: string;
-  env: EnvVar[]; // todo: env must have DESTINATION_CONTAINER and DESTINATION_CONTAINER
-  envFrom?: EnvFromSource[];
-  from: "dev" | "prod";
-  to: "dev" | "prod";
+interface AzureVolumePath {
+  container: string;
+  secretName?: string;
+  volume: string;
 }
-
-const getProjectSecretNamespace = (project: string) => `${project}-secret`;
 
 const restoreContainerScript = readFileSync(
   join(__dirname, "./restore-container.job.script.sh"),
   "utf8"
 );
 
-export const restoreContainerJob = ({
-  project,
-  env = [],
-  envFrom = [],
-  from,
-  to,
-}: RestoreJobArgs): Job => {
-  const ciEnv = environments(process.env);
-  const secretNamespace = getProjectSecretNamespace(project);
-  const jobEnv = scriptEnv(project, from, to);
+export const restoreContainerJob = (
+  name: string,
+  {
+    from,
+    to,
+  }: {
+    from: AzureVolumePath;
+    to: AzureVolumePath;
+  }
+): Job => {
+  const jobName = generate(`restore-container-${name}`);
+  const env = environments(process.env);
 
-  const jobName = generate(`restore-container-${ciEnv.branch}`);
+  const projectSlug = env.projectName.replace(/-/g, "");
+
+  const azurestorageaccountkey = "azurestorageaccountkey";
+  const sourceSecretName = `azure-${projectSlug}${from.volume}-volume`;
+  const destSecretName = `azure-${projectSlug}${to.volume}-volume`;
+  const azurestorageaccountname = "azurestorageaccountname";
   return new Job({
     metadata: {
-      annotations: { "kapp.k14s.io/update-strategy": "skip" },
+      annotations: {
+        "kapp.k14s.io/update-strategy": "skip",
+        ...env.metadata.annotations,
+      },
       labels: {
         component: "restore-container",
-        ...ciEnv.metadata.labels,
+        ...env.metadata.labels,
       },
       name: jobName,
-      namespace: secretNamespace,
+      namespace: env.metadata.namespace.name,
     },
     spec: {
       backoffLimit: 0,
@@ -55,8 +59,53 @@ export const restoreContainerJob = ({
           containers: [
             {
               command: ["sh", "-c", restoreContainerScript],
-              env: [...jobEnv, ...env],
-              envFrom,
+              env: [
+                {
+                  name: "SOURCE_CONTAINER",
+                  value: from.container,
+                },
+                {
+                  name: "SOURCE_ACCOUNT_NAME",
+                  valueFrom: {
+                    secretKeyRef: {
+                      key: azurestorageaccountname,
+                      name: sourceSecretName,
+                    },
+                  },
+                },
+                {
+                  name: "SOURCE_ACCOUNT_KEY",
+                  valueFrom: {
+                    secretKeyRef: {
+                      key: azurestorageaccountkey,
+                      name: sourceSecretName,
+                    },
+                  },
+                },
+                //
+                {
+                  name: "DESTINATION_CONTAINER",
+                  value: to.container,
+                },
+                {
+                  name: "DESTINATION_ACCOUNT_NAME",
+                  valueFrom: {
+                    secretKeyRef: {
+                      key: azurestorageaccountname,
+                      name: destSecretName,
+                    },
+                  },
+                },
+                {
+                  name: "DESTINATION_ACCOUNT_KEY",
+                  valueFrom: {
+                    secretKeyRef: {
+                      key: azurestorageaccountkey,
+                      name: destSecretName,
+                    },
+                  },
+                },
+              ],
               image: "mcr.microsoft.com/azure-cli:2.15.1",
               imagePullPolicy: "IfNotPresent",
               name: "restore-db",
@@ -79,104 +128,3 @@ export const restoreContainerJob = ({
     },
   });
 };
-
-function scriptEnv(project: string, from: string, to: string) {
-  const projectSlug = project.replace(/-/g, "");
-  const jobEnv = [];
-  // create needed env vars depending on the environemnt
-  if (from === "prod") {
-    jobEnv.push(
-      new EnvVar({
-        name: "SOURCE_ACCOUNT_NAME",
-        valueFrom: {
-          secretKeyRef: {
-            key: "azurestorageaccountname",
-            name: `azure-${projectSlug}prod-volume`,
-          },
-        },
-      })
-    );
-    jobEnv.push(
-      new EnvVar({
-        name: "SOURCE_ACCOUNT_KEY",
-        valueFrom: {
-          secretKeyRef: {
-            key: "azurestorageaccountkey",
-            name: `azure-${projectSlug}prod-volume`,
-          },
-        },
-      })
-    );
-  } else {
-    jobEnv.push(
-      new EnvVar({
-        name: "SOURCE_ACCOUNT_NAME",
-        valueFrom: {
-          secretKeyRef: {
-            key: "azurestorageaccountname",
-            name: `azure-${projectSlug}dev-volume`,
-          },
-        },
-      })
-    );
-    jobEnv.push(
-      new EnvVar({
-        name: "SOURCE_ACCOUNT_KEY",
-        valueFrom: {
-          secretKeyRef: {
-            key: "azurestorageaccountkey",
-            name: `azure-${projectSlug}dev-volume`,
-          },
-        },
-      })
-    );
-  }
-  if (to === "prod") {
-    jobEnv.push(
-      new EnvVar({
-        name: "DESTINATION_ACCOUNT_NAME",
-        valueFrom: {
-          secretKeyRef: {
-            key: "azurestorageaccountname",
-            name: `azure-${projectSlug}prod-volume`,
-          },
-        },
-      })
-    );
-    jobEnv.push(
-      new EnvVar({
-        name: "DESTINATION_ACCOUNT_KEY",
-        valueFrom: {
-          secretKeyRef: {
-            key: "azurestorageaccountkey",
-            name: `azure-${projectSlug}prod-volume`,
-          },
-        },
-      })
-    );
-  } else {
-    jobEnv.push(
-      new EnvVar({
-        name: "DESTINATION_ACCOUNT_NAME",
-        valueFrom: {
-          secretKeyRef: {
-            key: "azurestorageaccountname",
-            name: `azure-${projectSlug}dev-volume`,
-          },
-        },
-      })
-    );
-    jobEnv.push(
-      new EnvVar({
-        name: "DESTINATION_ACCOUNT_KEY",
-        valueFrom: {
-          secretKeyRef: {
-            key: "azurestorageaccountkey",
-            name: `azure-${projectSlug}dev-volume`,
-          },
-        },
-      })
-    );
-  }
-  return jobEnv;
-}
