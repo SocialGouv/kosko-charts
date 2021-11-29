79138777ffee88b1ecd8f4113d05dcfe12dbb171
Jo <jo@surikat.pro>
2021-11-29 17:20:13 +0100

    feat(pg): create-db-secret + create-db-user (#828)
    * chore(refacto): implement decoupled env-slug +lint
    
    * feat(pg): create-db-secret + create-db-user
    
    * fix(build): remove default arg and fix type
    
    * feat(pg): add autodevops templates
    
    * Update src/components/pg/README.md
    
    Co-authored-by: Julien Bouquillon <contact@revolunet.com>
    
    * Update src/components/pg/README.md
    
    Co-authored-by: Julien Bouquillon <contact@revolunet.com>
    
    * Update src/components/pg/create-db-secret.job.ts
    
    Co-authored-by: Julien Bouquillon <contact@revolunet.com>
    
    * Update src/components/pg/create-db-user.job.ts
    
    Co-authored-by: Julien Bouquillon <contact@revolunet.com>
    
    Co-authored-by: Julien Bouquillon <contact@revolunet.com>

---
+++ src/components/pg/README.md
@@ -0,0 +1,25 @@
+# PG Component
+
+## Usage
+
+Ensure secret containing random secure password for db exists
+
+```ts
+// in .k8s/components/create-db-secret.ts
+import createDbSecretJob from "@socialgouv/kosko-charts/components/pg/create-db-secret.job";
+
+
+// create a new secret and put it in the namespace
+const job = createDbSecretJob()
+export default [job];
+```
+
+Create db and user using password from secret
+
+```ts
+// in .k8s/components/create-db-user.ts
+import createDbUserJob from "@socialgouv/kosko-charts/components/pg/create-db-user.job";
+
+const job = createDbUserJob()
+export default [job];
+```
---
+++ src/components/pg/create-db-secret.job.ts
@@ -0,0 +1,58 @@
+import environments from "@socialgouv/kosko-charts/environments";
+import type { CIEnv } from "@socialgouv/kosko-charts/types";
+import { Job } from "kubernetes-models/batch/v1";
+
+const SOCIALGOUV_DOCKER_IMAGE = "ghcr.io/socialgouv/docker/azure-db";
+// renovate: datasource=docker depName=ghcr.io/socialgouv/docker/azure-db versioning=6.63.0
+const SOCIALGOUV_DOCKER_VERSION = "6.63.0";
+
+export const createDbSecretJob = ({
+  pgPasswordSecretKeyRef = "pgpassword",
+  ciEnv,
+}: {
+  pgPasswordSecretKeyRef?: string;
+  ciEnv: CIEnv;
+}): Job => {
+  return new Job({
+    metadata: {
+      annotations: ciEnv.metadata.annotations,
+      labels: ciEnv.metadata.labels,
+      name: `create-db-secret-${ciEnv.shortSha}`,
+      namespace: ciEnv.metadata.namespace.name,
+    },
+    spec: {
+      template: {
+        metadata: {},
+        spec: {
+          containers: [
+            {
+              args: ["create-db-secret $"],
+              command: ["sh", "-c"],
+              env: [
+                {
+                  name: "K8S_NS",
+                  value: ciEnv.metadata.namespace.name,
+                },
+                {
+                  name: "PGPASSWORD_SECRET_NAME",
+                  value: pgPasswordSecretKeyRef,
+                },
+              ],
+              image: `${SOCIALGOUV_DOCKER_IMAGE}:${SOCIALGOUV_DOCKER_VERSION}`,
+              imagePullPolicy: "IfNotPresent",
+              name: "create-db-secret",
+            },
+          ],
+          restartPolicy: "OnFailure",
+        },
+      },
+      ttlSecondsAfterFinished: 86400,
+    },
+  });
+};
+
+export default (): [Job] => {
+  const ciEnv = environments(process.env);
+  const job = createDbSecretJob({ ciEnv });
+  return [job];
+};
