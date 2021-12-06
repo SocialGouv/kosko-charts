a608b71c9eff1bf7fadce490f3ed0edf115d2c69
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2021-12-06 23:15:37 +0100

    feat(autodevops): enable netpols

--- templates/autodevops/utils/config.ts
+++ templates/autodevops/utils/config.ts
@@ -9,20 +9,34 @@ interface Probes {
 
 interface ConfigTypes {
   name: string;
+  /** app or static */
   type: string;
+  /** force deployment sudomain */
   subdomain: string;
+  /** deploy an hasura backend */
   hasura?: boolean | "exposed";
+  /** enable azure-pg : wait-pg initContainer and secrets */
   azurepg?: boolean;
+  /** define a custom pgHost for development */
   pgHostDev?: string;
+  /** redefines k8s probes */
   probes?: Probes;
+  /** redefines k8s resources */
   resources?: IIoK8sApiCoreV1ResourceRequirements;
+  /** redefines default probes path (/healthz) */
   probesPath?: string;
+  /** override ingress annotations */
   ingress?: {
     annotations?: Record<string, string>;
   };
+  /** ghcr or harbor */
   registry?: string;
+  /** project name */
   project?: string;
+
   restoreDb?: string;
+  /** set to false to deisable default network policy */
+  netpol?: boolean;
 }
 
 const Config = async (): Promise<ConfigTypes> => {
