5231303282536eaec3be286413c26e9333712ff4
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2021-12-27 11:08:37 +0100

    fix: add oauth2-proxy-tests

---
+++ e2e/templates/autodevops/app-oauth2-proxy/__tests__/kosko generate --env dev.ts
@@ -0,0 +1,46 @@
+// changes the cwd to a tmp folder
+import "templates/autodevops/utils/mock-directory"; //
+
+import { config } from "dotenv";
+import { KOSKO_BIN, template, TIMEOUT } from "e2e/templates/helpers";
+import execa from "execa";
+import { join, resolve } from "path";
+import { copy, copyFile, mkdir } from "fs-extra";
+
+//
+
+const cwd = template("autodevops");
+
+console.log("cwd", cwd);
+
+test(
+  "app: kosko generate --dev",
+  async () => {
+    const gitlabEnv = config({
+      path: resolve(cwd, "./environments/.gitlab-ci.env"),
+    }).parsed;
+
+    const env = {
+      ...gitlabEnv,
+      SOCIALGOUV_CONFIG_PATH: __dirname + "/config.json",
+    };
+
+    const destFolder = join(process.cwd(), "environments");
+    const sourceFolder = join(__dirname, "environments");
+
+    console.log(destFolder);
+    console.log(process.cwd());
+
+    await copy(sourceFolder, destFolder);
+
+    // Required to allow seemless integration code example
+    const result = await execa.node(KOSKO_BIN, ["generate", "--env", "dev"], {
+      cwd: process.cwd(),
+      env,
+    });
+
+    expect(result.stdout).toMatchSnapshot();
+    expect(result.exitCode).toEqual(0);
+  },
+  TIMEOUT
+);
