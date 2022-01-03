0ee847e8d4cf61dae92e87d0936142b6b6140c15
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2022-01-03 22:42:44 +0100

    fix

--- src/components/oauth2-proxy/index.test.ts
+++ src/components/oauth2-proxy/index.test.ts
@@ -15,7 +15,7 @@ const mockSealedSecret = new SealedSecret({
 jest.mock("@kosko/yaml", () => {
   return {
     loadFile:
-      (path: string, { transform }) =>
+      (path: string, { transform }: { transform: Function }) =>
       (): Manifest[] | null => {
         if (/configmap.yml$/.exec(path)) {
           return [transform(mockConfigMap)] as Manifest[];
