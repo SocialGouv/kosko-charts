86cfdaf7a3f736a0d0a15396ab3b5eb5c49705b5
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2022-02-10 08:53:19 +0100

    fix(oauth2): allow to override config

--- src/components/oauth2-proxy/index.test.ts
+++ src/components/oauth2-proxy/index.test.ts
@@ -1,3 +1,4 @@
+import { default as koskoEnv } from "@kosko/env";
 import { SealedSecret } from "@kubernetes-models/sealed-secrets/bitnami.com/v1alpha1";
 import environmentMock from "@socialgouv/kosko-charts/environments/index.mock";
 import { ConfigMap } from "kubernetes-models/v1";
@@ -11,6 +12,21 @@ const mockConfigMap = new ConfigMap({
 const mockSealedSecret = new SealedSecret({
   metadata: { name: "some-sealed-secret" },
 });
+
+// jest will mock any import of the fs module
+jest.mock("fs", () => {
+  const fs = jest.requireActual("fs");
+  return {
+    ...fs,
+    existsSync: () => true,
+  };
+});
+
+beforeEach(() => {
+  jest.resetAllMocks();
+});
+
+koskoEnv.env = "dev";
 
 jest.mock("@kosko/yaml", () => {
   return {
@@ -63,4 +79,67 @@ test("should return dev manifests with custom config", async () => {
     upstream: "http://target:123",
   });
   expect(manifests).toMatchSnapshot();
+});
+
+test("should return dev manifests with custom envFrom", async () => {
+  process.env.GITHUB_SHA = "123";
+  process.env.GITHUB_REF = "456";
+  process.env.GITHUB_JOB = "777";
+  process.env.GITHUB_RUN_ID = "888";
+  process.env.GITHUB_REPOSITORY = "socialgouv/test";
+  process.env.SOCIALGOUV_BASE_DOMAIN = "fabrique.social.gouv.fr";
+
+  jest.doMock("@socialgouv/kosko-charts/environments", () => environmentMock);
+
+  const expectedEnvFrom = [
+    {
+      secretRef: { name: "another-secret" },
+    },
+    {
+      configMapRef: { name: "another-configmap" },
+    },
+  ];
+  const manifests = await create({
+    config: {
+      container: {
+        envFrom: expectedEnvFrom,
+      },
+      subDomainPrefix: "metabase-",
+    },
+    upstream: "http://target:123",
+  });
+
+  // @ts-ignore
+  const containerEnvFrom = manifests.find((m) => m.kind === "Deployment").spec
+    .template.spec.containers[0].envFrom;
+  expect(containerEnvFrom).toEqual(expectedEnvFrom);
+});
+
+test("should return proxy manifests with custom env", async () => {
+  process.env.GITHUB_SHA = "123";
+  process.env.GITHUB_REF = "456";
+  process.env.GITHUB_JOB = "777";
+  process.env.GITHUB_RUN_ID = "888";
+  process.env.GITHUB_REPOSITORY = "socialgouv/test";
+  process.env.SOCIALGOUV_BASE_DOMAIN = "fabrique.social.gouv.fr";
+
+  jest.doMock("@socialgouv/kosko-charts/environments", () => environmentMock);
+
+  const manifests = await create({
+    config: {
+      container: {
+        env: [
+          {
+            name: "CUSTOM_VAR",
+            value: "HELLO",
+          },
+        ],
+      },
+    },
+    upstream: "http://target:123",
+  });
+  //@ts-expect-error
+  const containerEnv = manifests.find((m) => m.kind === "Deployment").spec
+    .template.spec.containers[0].env;
+  expect(containerEnv).toMatchSnapshot();
 });
--- src/components/oauth2-proxy/index.ts
+++ src/components/oauth2-proxy/index.ts
@@ -1,6 +1,7 @@
 import koskoEnv from "@kosko/env";
 import { loadFile } from "@kosko/yaml";
 import type { SealedSecret } from "@kubernetes-models/sealed-secrets/bitnami.com/v1alpha1";
+import { existsSync } from "fs";
 import type { ConfigMap } from "kubernetes-models/v1";
 import { EnvVar } from "kubernetes-models/v1";
 import path from "path";
@@ -20,21 +21,27 @@ const OAUTH2_PROXY_VERSION = "v7.2.0";
 
 // load some YAML from user env
 const loadEnvYaml = async (fileName: string) => {
-  return (
-    await loadFile(
-      path.join(koskoEnv.cwd, `environments/${koskoEnv.env}/${fileName}`),
-      {
-        transform: (manifest) => {
-          // force fix namespace
-          const ciEnv = environments(process.env);
-          if (manifest.metadata) {
-            manifest.metadata.namespace = ciEnv.metadata.namespace.name;
-          }
-          return manifest;
-        },
+  const yamlPath = path.join(
+    koskoEnv.cwd,
+    `environments/${koskoEnv.env}/${fileName}`
+  );
+
+  if (!existsSync(yamlPath)) {
+    console.info(`does not exist : ${yamlPath}`);
+    return [];
+  }
+  const manifests = await loadFile(yamlPath, {
+    transform: (manifest) => {
+      // force fix namespace
+      const ciEnv = environments(process.env);
+      if (manifest.metadata) {
+        manifest.metadata.namespace = ciEnv.metadata.namespace.name;
       }
-    )()
-  )[0];
+      return manifest;
+    },
+  })();
+
+  return manifests.length && manifests[0];
 };
 
 /*
@@ -49,21 +56,17 @@ export const create = async ({ upstream, config = {} }: ProxyParams) => {
   const configMap = (await loadEnvYaml(
     "oauth2-proxy-configmap.yml"
   )) as ConfigMap;
+
   // expect dedicated secret
   const sealedSecret = (await loadEnvYaml(
     "oauth2-proxy-sealed-secret.yml"
   )) as SealedSecret;
 
+  // oauth2 containers
   const manifests = await appCreate("proxy", {
     config: {
+      ...config,
       container: {
-        args: ["--upstream", upstream],
-        env: [
-          new EnvVar({
-            name: "OAUTH2_PROXY_HTTP_ADDRESS",
-            value: "0.0.0.0:4180",
-          }),
-        ],
         envFrom: [
           {
             configMapRef: { name: configMap.metadata?.name },
@@ -71,6 +74,15 @@ export const create = async ({ upstream, config = {} }: ProxyParams) => {
           {
             secretRef: { name: sealedSecret.metadata?.name },
           },
+        ],
+        ...config.container,
+        args: ["--upstream", upstream],
+        env: [
+          new EnvVar({
+            name: "OAUTH2_PROXY_HTTP_ADDRESS",
+            value: "0.0.0.0:4180",
+          }),
+          ...(config.container?.env ?? []),
         ],
         livenessProbe: {
           httpGet: {
@@ -93,7 +105,6 @@ export const create = async ({ upstream, config = {} }: ProxyParams) => {
       },
       containerPort: 4180,
       image: `quay.io/oauth2-proxy/oauth2-proxy:${OAUTH2_PROXY_VERSION}`,
-      ...config,
     },
     env: koskoEnv,
   });
@@ -110,5 +121,5 @@ export const create = async ({ upstream, config = {} }: ProxyParams) => {
     deployment,
   });
 
-  return [configMap, sealedSecret, manifests];
+  return [configMap, sealedSecret, ...manifests];
 };
